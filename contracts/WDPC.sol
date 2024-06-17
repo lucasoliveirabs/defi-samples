a4e381e144f5afb7732889019b649953f7920bfc
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-06-17 20:49:30 -0300

    feat: create get ETH/USD price function, precision  adjusted value returned

--- contracts/WDPC.sol
+++ contracts/WDPC.sol
@@ -2,13 +2,18 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract WDPC is ERC20 {
 
     event Deposit(address indexed account, uint256 amount);
     event Withdraw(address indexed account, uint256 amount);
     
-    constructor() ERC20("Wrapped Ether", "WDPC"){}
+    constructor() ERC20("Wrapped Ether Dollar-Pegged Coin", "WDPC"){
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
 
     receive() external payable {
         deposit();
@@ -21,5 +26,21 @@ contract WDPC is ERC20 {
     function deposit() public payable {
         _mint(msg.sender, msg.value);
         emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    function getLatestETHUSDPrice() public view returns (int) {
+        (
+            ,
+            int answer,
+            ,
+            ,
+        ) = dataFeed.latestRoundData();
+        return answer / 10 ** 8;
     }
 }
