237cdc8758cd75f523d9aaba69b8ce68a86c15ba
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-06-17 20:43:56 -0300

    feat: create deposit related functions

--- contracts/WDPC.sol
+++ contracts/WDPC.sol
@@ -7,5 +7,19 @@ contract WDPC is ERC20 {
 
     event Deposit(address indexed account, uint256 amount);
     event Withdraw(address indexed account, uint256 amount);
+    
+    constructor() ERC20("Wrapped Ether", "WDPC"){}
 
+    receive() external payable {
+        deposit();
+    }
+    
+    fallback() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
 }
